0bba2971c007c91f32081b883f87c16302f3e283
Admin <Admin@DESKTOP-ABPT33U>
2026-09-14 01:35:49 +0300

    Создана структура Pipe (труба) с полями. Создан экземпляр "myPipe" и считаны данные (километровая отметка, длина, диаметр, ремонт). В конце тела проекта произведена проверка, что все записалось.

--- ConsoleApp1/ConsoleApp1/ConsoleApp1.cpp
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1.cpp
@@ -3,9 +3,38 @@
 #include <string>
 #include <limits>
 
+struct Pipe {
+    std::string name;
+    double length;
+    double diameter;
+    bool inRepair;
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Pipe myPipe;
+    std::cout << "Введите километровую отметку (name): ";
+    std::cin.ignore();
+    std::getline(std::cin, myPipe.name);
+
+    std::cout << "Введите длину трубы в км (length): ";
+    std::cin >> myPipe.length;
+
+    std::cout << "Введите диаметр трубы в мм (diameter): ";
+    std::cin >> myPipe.diameter;
+
+    int repairFlag;
+    std::cout << "Труба в ремонте? (inRepair, 1 - да, 0 - нет): ";
+    std::cin >> repairFlag;
+    myPipe.inRepair = repairFlag;
+
+    std::cout << "\nВведённые данные:\n";
+    std::cout << "name = " << myPipe.name<< "\n";
+    std::cout << "length = " << myPipe.length << "\n";
+    std::cout << "diameter = " << myPipe.diameter << "\n";
+    std::cout << "inRepair = " << myPipe.inRepair << "\n";
+
+    return 0;
 }
 
 
